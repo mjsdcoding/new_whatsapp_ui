56b13ad1c0289d5ee46d8166a7cf3107579dd617
donvovo2 <38768072+donvovo2@users.noreply.github.com>
2023-02-16 11:59:13 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        drawerTheme: const DrawerThemeData(
+          scrimColor: Colors.transparent,
+        ),
+      ),
+      title: 'Flutter Demo',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> _key = GlobalKey(); // Create a key
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
